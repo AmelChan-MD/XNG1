802fc1595fd141757b9fc1db82fd5d26e7e73201
FeliciaFebbyutari <123058834+AmelChan-MD@users.noreply.github.com>
2026-03-04 19:53:43 +0700

    Fix import statement for baileys module

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const baileys = (await import("@adiwajshing/baileys")
+const baileys = await import("@adiwajshing/baileys");
 const makeWASocket = baileys.default;
 const {
   useMultiFileAuthState,
